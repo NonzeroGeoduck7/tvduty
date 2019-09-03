c6fdfc081f3b975b82f05aff3746033edacb3d27
NonzeroGeoduck7 <andreasroth@hispeed.ch>
2019-09-04 00:41:01 +0200

    remove pushalert code in <head>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import {Helmet} from "react-helmet";
 import './App.css';
 
 import SeriesTable from './components/SeriesTable'
@@ -8,15 +7,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-		<Helmet
-		
-            script={[{ 
-			  type: 'text/javascript', 
-		      innerHTML: '(function(d, t) {var g = d.createElement(t),s = d.getElementsByTagName(t)[0];g.src = "https://cdn.pushalert.co/integrate_8927437e76ea83afc8a090882ab0a679.js";                        s.parentNode.insertBefore(g, s);}(document, "script"))' 
-			}]} 
-		
-		/>
-	  
         <SeriesTable />
       </div>
     )

c7fe9b1270d006db74b435ed9b384dd393a8ce8d
Andreas <andreasroth@hispeed.ch>
2019-09-29 17:37:44 +0200

    seriesTable grid layout

--- src/components/SeriesElement.js
+++ src/components/SeriesElement.js
@@ -1,35 +1,39 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Link } from "react-router-dom"
 
 
 function SeriesElement(props) {
-
-  function func(text) {
-  	console.log("func: "+text)
-  }
-  
-  return (
-	  <div>
-	    <Link to={"/series/"+props.extId}>
-          <img height={props.height} width={props.height*3/4}
-	  		 src={props.poster}
-	         alt={'poster_' + props.title}
-	  		 onClick={() => func(props.title)}
-	      />
-	  	</Link>
-		  
-		  <label>
-			  {props.title} -> ep: {props.currentEpisode}/? ({props.currentEpisode/1}%)
-		  </label>
-	  </div>
-  );
+	
+	function func(text) {
+		console.log("func: "+text)
+	}
+	
+  	return (
+	  	<div>
+		  	<div>
+				<Link to={"/series/"+props.extId}>
+					<img height={props.height} width={props.height*3/4}
+						src={props.poster}
+						alt={'poster_' + props.title}
+						onClick={() => func(props.title)}
+					/>
+	  			</Link>
+			</div>
+			<div>
+				<label>
+					{props.title} -> ep: {props.currentEpisode}/? ({props.currentEpisode/1}%)
+				</label>
+			</div>
+		
+	  	</div>
+  	)
 }
 
 SeriesElement.propTypes = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   height: PropTypes.number.isRequired,
-};
+}
 
-export default SeriesElement;
+export default SeriesElement
--- src/components/SeriesTable.js
+++ src/components/SeriesTable.js
@@ -1,68 +1,71 @@
 // SeriesTable.js
 import React, { useState, useEffect } from 'react'
 import SeriesElement from './SeriesElement'
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import StackGrid from "react-stack-grid"
 
-import { useAuth0 } from "../react-auth0-wrapper";
+import { useAuth0 } from "../react-auth0-wrapper"
 
 function getWindowDimensions() {
-	const { innerWidth: width, innerHeight: height } = window;
-	return {
-	  width,
-	  height
-	};
-  }
-
-function SeriesTable() {
-	
-  let [seriesList, setSeriesList] = useState([])
-  let [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  const { user } = useAuth0();
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+    const { innerWidth: width, innerHeight: height } = window
+    return {
+        width,
+        height
     }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  // replaces componentDidMount -> reload when user.sub changes
-  useEffect(() => {
-    console.log("useEffect method completed, showsTable updated.")
-	
-	  // Fetch the Series from the database
-	  fetch('/.netlify/functions/seriesRead')
-		.then(res => res.json())
-		.then(response => {
-		  let filtered = response.data.filter(e=>e.userseries.filter(e1=>e1.userId===user.sub).length > 0)
-		  setSeriesList(filtered)
-		})
-		.catch(err => console.log('Error retrieving products: ', err))
-	  
-  }, [user.sub]);
-	
-  const { width, height } = windowDimensions;
-
-  return (
-    <div>
-  	  <p>Screen size: {width} x {height}</p>
-      {seriesList.map(c => 
-        <SeriesElement
-          key={c.extId}
-          height={height / 3}
-          currentEpisode={c.userseries[0].currentEpisode}
-          title={c.title}
-          poster={c.poster}
-          extId={c.extId} />
-      )}
-
-  	  <Link to="/add">
-  	    <button>&#43;</button>
-  	  </Link>
-    </div>
-  )
 }
 
-export default SeriesTable;
+function SeriesTable() {
+    
+    let [seriesList, setSeriesList] = useState([])
+    let [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+    const { user } = useAuth0()
+    useEffect(() => {
+        function handleResize() {
+          setWindowDimensions(getWindowDimensions())
+        }
+
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
+    // replaces componentDidMount -> reload when user.sub changes
+    useEffect(() => {
+        console.log("useEffect method completed, showsTable updated.")
+      
+        // Fetch the Series from the database
+        fetch('/.netlify/functions/seriesRead')
+        .then(res => res.json())
+        .then(response => {
+          let filtered = response.data.filter(e=>e.userseries.filter(e1=>e1.userId===user.sub).length > 0)
+          setSeriesList(filtered)
+        })
+        .catch(err => console.log('Error retrieving products: ', err))
+    }, [user.sub])
+    
+    const { width, height } = windowDimensions
+
+    return (
+        <div>
+            <p>Screen size: {width} x {height}</p>
+
+            <Link to="/add">
+                <button>&#43;</button>
+            </Link>
+
+            <StackGrid columnWidth={330}>
+                {seriesList.map(c => 
+                    <SeriesElement
+                        key={c.extId}
+                        height={height / (3)}
+                        currentEpisode={c.userseries[0].currentEpisode}
+                        title={c.title}
+                        poster={c.poster}
+                        extId={c.extId} />
+                )}
+            </StackGrid>
+        </div>
+    )
+}
+
+export default SeriesTable
